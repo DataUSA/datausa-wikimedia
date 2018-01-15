841a5618498a27601b8435b6a8d5d4b8249f188a
Dave Landry <landry.dave@gmail.com>
2018-01-15 17:21:47 -0500

    fixes upload file sorting and printouts

--- upload.js
+++ upload.js
@@ -3,29 +3,37 @@ const Bot = require("nodemw"),
         protocol: "https",
         server: "commons.wikimedia.org",
         path: "/w",
-        debug: true,
+        debug: false,
         username: process.env.WIKI_USERNAME,
         password: process.env.WIKI_PASSWORD,
         userAgent: "DataUSA Uploader (https://datausa.io; hello@datausa.io)"
       }),
       fs = require("fs");
 
+let total;
+
 function upload(files) {
 
   const fileName = files.pop();
+  process.stdout.write(`\nUploaded ${total - files.length} of ${total}`);
   const {title} = require(fileName.replace("svg", "js"));
+
   fs.readFile(fileName, "utf8", (err, svg) => {
     if (err) throw err;
     fs.readFile(fileName.replace("svg", "txt"), "utf8", (err, txt) => {
       if (err) throw err;
-      client.getArticle(`File:${title}.svg`, err => {
-        if (err) throw err;
-        client.upload(title, svg, "updates SVG", () => {
-          client.edit(`File:${title}.svg`, txt, "updates description", () => {
-            console.log(`uploaded: ${title}`);
-            if (files.length) upload(files);
-            else console.log("done!");
-          });
+      client.upload(title, svg, "updates SVG", () => {
+        client.edit(`File:${title}.svg`, txt, "updates description", err => {
+          if (err) throw err;
+
+          process.stdout.clearLine();
+          process.stdout.cursorTo(0);
+          process.stdout.write(title);
+
+          if (files.length) {
+            setTimeout(() => upload(files), 7500);
+          }
+          else console.log("done!");
         });
       });
     });
@@ -37,6 +45,10 @@ client.logIn(err => {
   if (err) throw err;
   fs.readdir("./exports", (err, files) => {
     if (err) throw err;
-    upload(files.filter(file => file.includes(".svg")).map(file => `./exports/${file}`));
+    files = files.filter(file => file.includes(".svg"))
+      .sort((a, b) => b.localeCompare(a))
+      .map(file => `./exports/${file}`);
+    total = files.length;
+    upload(files);
   });
 });
